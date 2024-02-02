0a6145f5951664819a5650a8c568bda2a0d75756
anoy1010 <jojoletag@gmail.com>
2024-02-02 15:43:00 +0100

    correction page failed

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,6 @@
-import Footer from '@/Comps/Footer'
-import Header from '@/Comps/Header'
-import Image from 'next/image'
-import Link from 'next/link'
+
 import Acceuil from '@/app/pages/Home&personal/page'
-import Product from '@/Comps/product/page'
-import Cart from '@/Comps/cart/page'
-import Errorpage from './pages/Errorpage/page'
-import ProductPage from './pages/ecommerce/ProductPage/page'
-import Chekout from '@/Comps/checkout/page'
-import Success from './pages/Success'
-import Failed from './pages/failed'
+
 
 export default function Home() {
   return (
