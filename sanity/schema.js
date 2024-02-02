570d94c5613291fd3e9116d05b1c9e9862485a36
anoy1010 <jojoletag@gmail.com>
2024-02-02 15:48:44 +0100

    importation des schema

--- sanity/schema.js
+++ sanity/schema.js
@@ -2,10 +2,10 @@ import {blockContent} from './schemas/blockContent'
 import {category} from './schemas/category'
 import {post} from './schemas/post'
 import {author} from './schemas/author'
-import { banner } from './schemas/banner'
-import { product } from './schemas/product'
-import { user } from './schemas/user-schema'
-import { order } from './schemas/order-schema'
+import  banner  from './schemas/banner'
+import  product  from './schemas/product'
+import user  from './schemas/user-schema'
+import  order  from './schemas/order-schema'
 
 export const schema = {
   types: [user, order, banner, product],
