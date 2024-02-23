8a0d2466eea31f21b6bf03ba120d62ef7714c7e4
anoy1010 <jojoletag@gmail.com>
2024-02-23 13:52:32 +0100

    PME

--- src/app/pages/Smallbusiness/page.js
+++ src/app/pages/Smallbusiness/page.js
@@ -2,8 +2,8 @@
 
 import React, {useRef, useEffect, useState} from 'react'
 import Carousel from 'better-react-carousel'
-
-
+import Image from "next/image";
+ 
 import ButtonView from '@/Comps/elem/ButtonView'
 import Headtitle, { Headtitle1 } from '@/Comps/elem/Headtitle'
 import Button from '@/Comps/elem/Button'
@@ -54,8 +54,8 @@ function Smallbusiness() {
                 <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
                     <div className='text-center py-24 flex flex-col justify-center items-center '>
                         <h1 className='font-bold text-center text-[#4556F5] uppercase text-6xl 
-                        '> the new genreration of security </h1>
-                        <h3 className='font-thin text-lg text-white uppercase'>nous convenons pour tout entreprises avec une sécurité faite sur mesure</h3>
+                        '> La nouvelle ère de la sécurité. </h1>
+                        <h3 className='font-thin text-lg text-white uppercase'>nous convenons pour toutes entreprises avec une sécurité faite sur mesure</h3>
                     </div>
                     <div class="space-y-8 lg:grid lg:grid-cols-3 sm:gap-6 xl:gap-10 lg:space-y-0 places-content-center places-items-center container">
                         <CardSlideY>
@@ -207,22 +207,25 @@ function Smallbusiness() {
             <div className=''>
                 <HeadSlide>
                     <Headtitle 
-                    title="Question & answer"
-                    text="lorem ipsum pour ne pas avoir de problème"/>
+                    title="Quel sécurité pour votre magasin ?"
+                    text="SafeR est une offre de sécurité complète et personnalisée qui répond aux besoins spécifiques des petits et moyens commerces. "/>
                 </HeadSlide>
             </div>
             <div className='container flex gap-x-8 py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6'>
                 <CardScale>           
                     <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                    <svg className="w-7 h-7 text-gray-500 dark:text-gray-400 mb-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                        <path d="M18 5h-.7c.229-.467.349-.98.351-1.5a3.5 3.5 0 0 0-3.5-3.5c-1.717 0-3.215 1.2-4.331 2.481C8.4.842 6.949 0 5.5 0A3.5 3.5 0 0 0 2 3.5c.003.52.123 1.033.351 1.5H2a2 2 0 0 0-2 2v3a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1V7a2 2 0 0 0-2-2ZM8.058 5H5.5a1.5 1.5 0 0 1 0-3c.9 0 2 .754 3.092 2.122-.219.337-.392.635-.534.878Zm6.1 0h-3.742c.933-1.368 2.371-3 3.739-3a1.5 1.5 0 0 1 0 3h.003ZM11 13H9v7h2v-7Zm-4 0H2v5a2 2 0 0 0 2 2h3v-7Zm6 0v7h3a2 2 0 0 0 2-2v-5h-5Z"/>
-                    </svg>
+                    <Image
+                        src="/camera-de-surveillance.png"
+                        width={70}
+                        height={70}
+                        alt="camera-de-surveillance"
+                    />
                     <a href="#">
-                        <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Need a help in Claim?</h5>
+                        <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Vidéosurveillance</h5>
                     </a>
-                    <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">Go to this step by step guideline process on how to certify for your weekly benefits:</p>
+                    <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">Permet de surveiller les locaux et les biens de l&apos;entreprise à distance et de dissuader les criminels.</p>
                     <a href="#" className="inline-flex items-center text-blue-600 hover:underline">
-                        See our guideline
+                        Plus d&apos;informations
                         <svg className="w-3 h-3 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778"/>
                         </svg>
@@ -231,15 +234,18 @@ function Smallbusiness() {
                 </CardScale> 
                 <CardScale> 
                     <div className="max-w-sm p-6 bg-white border border-gray-200    rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                    <svg class="w-7 h-7 text-gray-500 dark:text-gray-400 mb-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                        <path d="M18 5h-.7c.229-.467.349-.98.351-1.5a3.5 3.5 0 0 0-3.5-3.5c-1.717 0-3.215 1.2-4.331 2.481C8.4.842 6.949 0 5.5 0A3.5 3.5 0 0 0 2 3.5c.003.52.123 1.033.351 1.5H2a2 2 0 0 0-2 2v3a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1V7a2 2 0 0 0-2-2ZM8.058 5H5.5a1.5 1.5 0 0 1 0-3c.9 0 2 .754 3.092 2.122-.219.337-.392.635-.534.878Zm6.1 0h-3.742c.933-1.368 2.371-3 3.739-3a1.5 1.5 0 0 1 0 3h.003ZM11 13H9v7h2v-7Zm-4 0H2v5a2 2 0 0 0 2 2h3v-7Zm6 0v7h3a2 2 0 0 0 2-2v-5h-5Z"/>
-                    </svg>
+                    <Image
+                        src="/controle-dacces.png"
+                        width={70}
+                        height={70}
+                        alt="controle-dacces"
+                    />
                     <a href="#">
-                        <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Need a help in Claim?</h5>
+                        <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Contrôle d&apos;acces</h5>
                     </a>
-                    <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">Go to this step by step guideline process on how to certify for your weekly benefits:</p>
+                    <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">Permet de limiter l&apos;accès aux locaux de l&apos;entreprise aux personnes autorisées et de connaître qui a eu accès. </p>
                     <a href="#" class="inline-flex items-center text-blue-600 hover:underline">
-                        See our guideline
+                        Plus d&apos;informations
                         <svg class="w-3 h-3 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778"/>
                         </svg>
@@ -248,21 +254,44 @@ function Smallbusiness() {
                 </CardScale> 
                 <CardScale> 
                     <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                        <svg class="w-7 h-7 text-gray-500 dark:text-gray-400 mb-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                            <path d="M18 5h-.7c.229-.467.349-.98.351-1.5a3.5 3.5 0 0 0-3.5-3.5c-1.717 0-3.215 1.2-4.331 2.481C8.4.842 6.949 0 5.5 0A3.5 3.5 0 0 0 2 3.5c.003.52.123 1.033.351 1.5H2a2 2 0 0 0-2 2v3a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1V7a2 2 0 0 0-2-2ZM8.058 5H5.5a1.5 1.5 0 0 1 0-3c.9 0 2 .754 3.092 2.122-.219.337-.392.635-.534.878Zm6.1 0h-3.742c.933-1.368 2.371-3 3.739-3a1.5 1.5 0 0 1 0 3h.003ZM11 13H9v7h2v-7Zm-4 0H2v5a2 2 0 0 0 2 2h3v-7Zm6 0v7h3a2 2 0 0 0 2-2v-5h-5Z"/>
-                        </svg>
+                    <Image
+                        src="/urgence.png"
+                        width={70}
+                        height={70}
+                        alt="urgence"
+                    />
                         <a href="#">
-                            <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Need a help in Claim?</h5>
+                            <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Alarme intrusion</h5>
                         </a>
-                        <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">Go to this step by step guideline process on how to certify for your weekly benefits:</p>
+                        <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">alerté en cas d&apos;intrusion dans les locaux de l&apos;entreprise et  Permet d'alerter une brigade d&apos;urgence en cas d'intrusion.</p>
                         <a href="#" class="inline-flex items-center text-blue-600 hover:underline">
-                            See our guideline
+                            Plus d&apos;informations
                             <svg class="w-3 h-3 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778"/>
                             </svg>
                         </a>
                     </div>
                 </CardScale> 
+                <CardScale> 
+                    <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                    <Image
+                        src="/incendie.png"
+                        width={70}
+                        height={70}
+                        alt="incendie"
+                    />
+                        <a href="#">
+                            <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Protection incendie</h5>
+                        </a>
+                        <p class="mb-3 font-normal text-gray-500 dark:text-gray-400"> Permet d&apos;être alerté en cas de départ de feu et Permet de lutter contre un incendie en cas de départ de feu.</p>
+                        <a href="#" class="inline-flex items-center text-blue-600 hover:underline">
+                           Plus d&apos;information
+                            <svg class="w-3 h-3 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778"/>
+                            </svg>
+                        </a>
+                    </div>
+                </CardScale>
 
             </div>
             <div className='py-24'>
@@ -275,27 +304,27 @@ function Smallbusiness() {
                     <div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
                     <div class="lg:pr-8 lg:pt-4">
                         <div class="lg:max-w-lg">
-                        <h2 class="text-base font-semibold leading-7 text-indigo-600">Deploy faster</h2>
-                        <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">A better workflow</p>
-                        <p class="mt-6 text-lg leading-8 text-gray-600">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</p>
+                        <h2 class="text-base font-semibold leading-7 text-indigo-600">Avantages</h2>
+                        <p class="mt-2 text-3xl font-bold tracking-tight uppercase text-gray-900 sm:text-4xl">La sécurité électronique</p>
+                        <p class="mt-6 text-lg leading-8 text-gray-600">La sécurité électronique est un élément essentiel pour la protection des petites entreprises en Côte d&apos;Ivoire. En choisissant les solutions les plus appropriées à vos besoins et en prenant les mesures nécessaires pour vous protéger , vous pouvez aussi contribuer à assurer la sécurité de votre entreprise et de ses biens.</p>
                         <dl class="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
-                            <div class="relative pl-9">
-                            <dt class="inline font-semibold text-gray-900">
-                                <svg class="absolute left-1 top-1 h-5 w-5 text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                <path fill-rule="evenodd" d="M5.5 17a4.5 4.5 0 01-1.44-8.765 4.5 4.5 0 018.302-3.046 3.5 3.5 0 014.504 4.272A4 4 0 0115 17H5.5zm3.75-2.75a.75.75 0 001.5 0V9.66l1.95 2.1a.75.75 0 101.1-1.02l-3.25-3.5a.75.75 0 00-1.1 0l-3.25 3.5a.75.75 0 101.1 1.02l1.95-2.1v4.59z" clip-rule="evenodd" />
-                                </svg>
-                                Push to deploy.
-                            </dt>
-                            <dd class="inline">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</dd>
-                            </div>
                             <div class="relative pl-9">
                             <dt class="inline font-semibold text-gray-900">
                                 <svg class="absolute left-1 top-1 h-5 w-5 text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                                 <path fill-rule="evenodd" d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z" clip-rule="evenodd" />
                                 </svg>
-                                SSL certificates.
+                                Protection contre les crimes et les incidents
                             </dt>
-                            <dd class="inline">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.</dd>
+                            <dd class="inline">La sécurité électronique peut dissuader les criminels et limiter les dégâts en cas d&apos;incident.</dd>
+                            </div>
+                            <div class="relative pl-9">
+                            <dt class="inline font-semibold text-gray-900">
+                            <svg class="absolute left-1 top-1 h-5 w-5 text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                <path fill-rule="evenodd" d="M5.5 17a4.5 4.5 0 01-1.44-8.765 4.5 4.5 0 018.302-3.046 3.5 3.5 0 014.504 4.272A4 4 0 0115 17H5.5zm3.75-2.75a.75.75 0 001.5 0V9.66l1.95 2.1a.75.75 0 101.1-1.02l-3.25-3.5a.75.75 0 00-1.1 0l-3.25 3.5a.75.75 0 101.1 1.02l1.95-2.1v4.59z" clip-rule="evenodd" />
+                                </svg>
+                                Réduction des coûts d&apos;assurance:
+                            </dt>
+                            <dd class="inline">Les compagnies d&apos;assurance peuvent proposer des réductions aux entreprises qui ont mis en place des mesures de sécurité électroniques.</dd>
                             </div>
                             <div class="relative pl-9">
                             <dt class="inline font-semibold text-gray-900">
@@ -303,14 +332,14 @@ function Smallbusiness() {
                                 <path d="M4.632 3.533A2 2 0 016.577 2h6.846a2 2 0 011.945 1.533l1.976 8.234A3.489 3.489 0 0016 11.5H4c-.476 0-.93.095-1.344.267l1.976-8.234z" />
                                 <path fill-rule="evenodd" d="M4 13a2 2 0 100 4h12a2 2 0 100-4H4zm11.24 2a.75.75 0 01.75-.75H16a.75.75 0 01.75.75v.01a.75.75 0 01-.75.75h-.01a.75.75 0 01-.75-.75V15zm-2.25-.75a.75.75 0 00-.75.75v.01c0 .414.336.75.75.75H13a.75.75 0 00.75-.75V15a.75.75 0 00-.75-.75h-.01z" clip-rule="evenodd" />
                                 </svg>
-                                Database backups.
+                                Renforcement de l&apos;image de l&apos;entreprise:
                             </dt>
-                            <dd class="inline">Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.</dd>
+                            <dd class="inline">La mise en place de solutions de sécurité électronique peut renforcer l&apos;image de l&apos;entreprise auprès des clients et des partenaires.</dd>
                             </div>
                         </dl>
                         </div>
                     </div>
-                    <img src='secu.jpg' className='h-full w-full bg-cover object-fit'/>
+                    <img src='/alexey-soucho-GHZnqk0VpPU-unsplash.jpg' className='h-full w-full bg-cover object-fit'/>
                     </div>
                 </div>
             </div>
@@ -318,8 +347,8 @@ function Smallbusiness() {
                 <div>
                     <HeadSlide>
                         <Headtitle1 
-                            title="we continullay to improve"
-                            text=" the art is the security in SAFER"
+                            title="Nous continuons toujours d'innover"
+                            text=" La sécurité est un art chez SafeR"
                         />
                     </HeadSlide>
                 </div>
